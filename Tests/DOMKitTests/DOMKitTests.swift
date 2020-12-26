0977a93683d9156c6fc57c22eb1600491a97dd3c
Max Desiatov <max@desiatov.com>
2020-12-26 21:35:44 +0000

    Add GitHub Actions workflow, assert in the test

--- Tests/DOMKitTests/DOMKitTests.swift
+++ Tests/DOMKitTests/DOMKitTests.swift
@@ -1,15 +1,17 @@
 import XCTest
-import JavaScriptKit
-@testable import DOMKit
+import DOMKit
 
 final class DOMKitTests: XCTestCase {
     func testExample() {
-        let document = DOMKit.Document(from: JSObject.global.document)!
+        let document = global.document
         let button = document.createElement(localName: "button")
-        button.textContent = "Hello, world"
+        button.textContent = "Hello, world!"
         button.addEventListener(type: "click") { event in
             (event.target as? HTMLElement)?.textContent = "Clicked!"
         }
         _ = document.querySelector(selectors: "body")?.appendChild(node: button)
+
+        let queriedButton = document.querySelector(selectors: "body button")
+        XCTAssertEqual(button.textContent, "Hello, world!")
     }
 }
