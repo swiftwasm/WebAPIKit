feb20ef578cfc973f5533a46acb3ab189f7f53f5
Max Desiatov <max@desiatov.com>
2020-12-27 20:00:34 +0000

    Assert on `queriedButton` not `button` in the test

--- Tests/DOMKitTests/DOMKitTests.swift
+++ Tests/DOMKitTests/DOMKitTests.swift
@@ -6,12 +6,9 @@ final class DOMKitTests: XCTestCase {
         let document = globalThis.document
         let button = document.createElement(localName: "button")
         button.textContent = "Hello, world!"
-        button.addEventListener(type: "click") { event in
-            (event.target as? HTMLElement)?.textContent = "Clicked!"
-        }
         _ = document.querySelector(selectors: "body")?.appendChild(node: button)
 
         let queriedButton = document.querySelector(selectors: "body button")
-        XCTAssertEqual(button.textContent, "Hello, world!")
+        XCTAssertEqual(queriedButton?.textContent, "Hello, world!")
     }
 }
