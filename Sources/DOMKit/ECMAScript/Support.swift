3d7c312832ae1fcbbcba52f992d214e956e0116b
Jed Fox <git@jedfox.com>
2022-03-30 16:08:09 -0400

    check in my changes to the iterator support (not working)

--- Sources/DOMKit/ECMAScript/Support.swift
+++ Sources/DOMKit/ECMAScript/Support.swift
@@ -59,22 +59,35 @@ public typealias Uint8ClampedArray = JSUInt8ClampedArray
 
 public class ValueIterableIterator<SequenceType: JSBridgedClass & Sequence>: IteratorProtocol where SequenceType.Element: ConstructibleFromJSValue {
     private var index: Int = 0
+    private let iterator: JSObject
+
+    public init(sequence: SequenceType) {
+        iterator = sequence.jsObject[JSObject.global.Symbol.object!.iterator]!().object!
+    }
+
+    public func next() -> SequenceType.Element? {
+        defer { index += 1 }
+        let value = iterator.next!()
+        guard value != .undefined else {
+            return nil
+        }
+
+        return value.fromJSValue()
+    }
+}
+
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+public class ValueIterableAsyncIterator<SequenceType: JSBridgedClass & AsyncSequence>: AsyncIteratorProtocol where SequenceType.Element: ConstructibleFromJSValue {
+    private var index: Int = 0
     private let sequence: SequenceType
 
     public init(sequence: SequenceType) {
         self.sequence = sequence
     }
 
-    public func next() -> SequenceType.Element? {
-        defer {
-            index += 1
-        }
-        let value = sequence.jsObject[index]
-        guard value != .undefined else {
-            return nil
-        }
-
-        return value.fromJSValue()
+    public func next() async -> SequenceType.Element? {
+        // TODO: implement
+        nil
     }
 }
 
