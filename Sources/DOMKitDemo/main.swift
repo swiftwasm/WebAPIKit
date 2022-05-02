3bb1463fbb7d3cb939df24ed4e8ba8cbadd9c091
Max Desiatov <max@desiatov.com>
2022-05-02 19:01:45 +0100

    Update Sources/DOMKitDemo/main.swift
    Co-authored-by: Jed Fox <git@jedfox.com>

--- Sources/DOMKitDemo/main.swift
+++ Sources/DOMKitDemo/main.swift
@@ -5,6 +5,6 @@ let document = globalThis.document
 
 let header = HTMLElement(from: document.createElement(localName: "h1"))!
 header.innerText = "Hello World!"
-_ = document.body?.appendChild(node: header)
+_ = document.body!.appendChild(node: header)
 
 console.log(data: "Hello, world!")
