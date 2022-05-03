76a6e253c111244da2265e9948d1c410d09cdf6f
Yuta Saito <kateinoigakukun@gmail.com>
2022-05-03 17:04:00 +0900

    Repair build

--- Sources/DOMKit/Support.swift
+++ Sources/DOMKit/Support.swift
@@ -1,6 +1,5 @@
 import JavaScriptKit
 import ECMAScript
-import DOMKitFileAPI
 
 /* TODO: fix this */
 public typealias __UNSUPPORTED_BIGINT__ = JSValue
@@ -18,8 +17,4 @@ public let globalThis = Window(from: JSObject.global.jsValue)!
 public typealias Uint8ClampedArray = JSUInt8ClampedArray
 // public typealias CSSColorValue_or_CSSStyleValue = CSSStyleValue
 // public typealias Any_CSSColorValue_or_CSSStyleValue = CSSStyleValue
-public typealias Blob_or_MediaSource = Blob
-public typealias HTMLOrSVGImageElement = HTMLImageElement
-public typealias HTMLOrSVGScriptElement = HTMLScriptElement
-public typealias BodyInit = XMLHttpRequestBodyInit
 public typealias CustomElementConstructor = JSFunction
