14827c6a0023d6d643868565afa62e03dc5c631f
Jed Fox <git@jedfox.com>
2023-01-28 16:32:44 -0500

    SwiftFormat, remove header comment

--- Sources/DOM/Customized.swift
+++ Sources/DOM/Customized.swift
@@ -1,15 +1,8 @@
-//
-//  Customized.swift
-//  
-//
-//  Created by Jed Fox on 2023-01-28.
-//
-
 import Foundation
 
-extension HTMLFormControlsCollection {
+public extension HTMLFormControlsCollection {
     // removes `override` since the superclass returns a more constrained type `Element`
-    @inlinable public func namedItem(name: String) -> Element_or_RadioNodeList? {
+    @inlinable func namedItem(name: String) -> Element_or_RadioNodeList? {
         let this = jsObject
         return this[Strings.namedItem].function!(this: this, arguments: [_toJSValue(name)]).fromJSValue()!
     }
